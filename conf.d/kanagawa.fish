9d4b53ae5fdd92b20cc345de23172ca443c24b99
uajqq <46248396+uajqq@users.noreply.github.com>
2023-11-25 14:57:57 -0500

    Add theme files and update license

--- conf.d/kanagawa.fish
+++ conf.d/kanagawa.fish
@@ -1,19 +1,23 @@
-# Gruvbox Color Palette
-set -l foreground ebdbb2
-set -l selection 282828 
-set -l comment 928374 
-set -l red fb4934
-set -l orange fe8019
-set -l yellow fabd2f
-set -l green b8bb26
-set -l cyan 8ec07c
-set -l blue 83a598
-set -l purple d3869b
+#!/usr/bin/fish
+
+# Kanagawa Fish shell theme
+# A template was taken and modified from Tokyonight:
+# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
+set -l foreground DCD7BA normal
+set -l selection 2D4F67 brcyan
+set -l comment 727169 brblack
+set -l red C34043 red
+set -l orange FF9E64 brred
+set -l yellow C0A36E yellow
+set -l green 76946A green
+set -l purple 957FB8 magenta
+set -l cyan 7AA89F cyan
+set -l pink D27E99 brmagenta
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
 set -g fish_color_command $cyan
-set -g fish_color_keyword $blue
+set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
@@ -23,7 +27,7 @@ set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
-set -g fish_color_escape $blue
+set -g fish_color_escape $pink
 set -g fish_color_autosuggestion $comment
 
 # Completion Pager Colors
